ed7b44e4efb347b1d57c54f76da8c3aad3c49a10
cashcheck <hnguyen1422@gmail.com>
2022-10-20 16:43:35 +0700

    jwt strategies working

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,23 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const userController = require("../controllers/user_controller");
+const adminStrategy = require("../strategies/jwt_admin");
+const userStrategy = require("../strategies/jwt_username");
+
+//configuriing different strategies for passport
+passport.use("admin", adminStrategy);
+passport.use("username", userStrategy);
 
 router.post("/register", userController.register);
 
 router.post("/login", userController.login);
 
-router.get("/protected");
+router.get(
+  "/protected",
+  passport.authenticate("admin", { session: false }),
+  (req, res, next) => {
+    res.status(200).json({ message: "welcome to the protected route" });
+  }
+);
 
 module.exports = router;
--- strategies/jwt_admin.js
+++ strategies/jwt_admin.js
@@ -4,8 +4,8 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.secret;
 
-module.exports = new JwtStrategy(opts, (req, payload, done) => {
-  if (username === "cashcheck") {
+module.exports = new JwtStrategy(opts, (payload, done) => {
+  if (payload.username === "cashcheck") {
     return done(null, true);
   }
   return done(null, false);
